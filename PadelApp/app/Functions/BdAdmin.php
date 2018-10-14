a8c86080ea21fc92b4c7aa65fd3824e3cc3abe2b
miferreiro <miguel.ferreiro.diaz@gmail.com>
2018-10-14 20:24:16 +0200

    Add credentials for BD

--- PadelApp/app/Functions/BdAdmin.php
+++ PadelApp/app/Functions/BdAdmin.php
@@ -10,7 +10,7 @@
 function ConectarBD() 
 	{
 		// se ejecuta la función de conexión mysqli y se recoge el manejador
-	    $mysqli = new mysqli("localhost", "userET3", "passET3", "IUET32017"); //maquina, user, pass, bd
+	    $mysqli = new mysqli("localhost", "user", "pass", "ABP2018"); //maquina, user, pass, bd
 		// si hay error en la conexión se muestra el mensaje de error
 		if ($mysqli->connect_errno) {
 			echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;//muestra mensaje de error
