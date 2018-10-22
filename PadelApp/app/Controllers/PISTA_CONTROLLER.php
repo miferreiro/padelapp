d5703841e84989889881735a8fe2706b057328ce
DESKTOP-M5AEJ9Q\david <davurin76@gmail.com>
2018-10-22 12:31:22 +0200

    pISTAS1

---
+++ PadelApp/app/Controllers/PISTA_CONTROLLER.php
@@ -0,0 +1,162 @@
+<?php
+
+session_start(); //solicito trabajar con la session
+include '../Functions/Authentication.php'; //incluye el contenido de la función de autentificación
+//Si no esta autenticado se redirecciona al index
+if (!IsAuthenticated()){
+	//Redireción al index
+ 	header('Location:../index.php');
+}
+
+include '../Models/PISTA_MODEL.php'; //incluye el contendio del modelo PISTAS
+include '../Views/PISTA/PISTA_SHOWALL.php'; //incluye la vista del showall
+include '../Views/PISTA/PISTA_EDIT.php'; //incluye la vista EDIT
+include '../Views/PISTA/PISTA_SHOWCURRENT.php'; //incluye la vista SEARCH
+
+
+//Esta función crea un objeto tipo PISTA_MODEL con los valores que se le pasan con $_REQUEST
+function get_data_form() {
+	
+	$idPista = $_REQUEST[ 'idPista' ]; //Variable que almacena el valor de idPista
+	$hora = $_REQUEST[ 'Hora' ]; //Variable que almacena el valor de Hora
+	$fecha = $_REQUEST[ 'Fecha' ]; //Variable que almacena el valor de Fecha
+	$disponibilidad = $_REQUEST[ 'Disponibilidad' ]; //Variable que almacena el valor de disponibilidad
+	$action = $_REQUEST[ 'action' ]; //Variable que almacena el valor de action
+    //Variable que almacena un modelo de PISTA
+	$PISTA = new PISTA_MODEL(
+		$idPista,
+		$hora,
+		$fecha,
+		$disponibilidad
+		
+	);//Creamos un objeto de pista con las variables que se han recibido del formulario
+	//Devuelve el valor del objecto model creado
+	
+	return $PISTA;
+}
+//Si la variable action no tiene contenido le asignamos ''
+if ( !isset( $_REQUEST[ 'action' ] ) ) {
+	$_REQUEST[ 'action' ] = '';
+	
+}
+//Estructura de control, que realiza un determinado caso dependiendo del valor action
+switch ( $_REQUEST[ 'action' ] ) {
+	case 'ADD'://Caso añadir
+		if ( !$_POST ) {//Si no se han recibido datos se envia a la vista del formulario ADD
+
+		
+         
+			new MESSAGE( 'La PISTA no tiene los permisos necesarios', '../Controllers/PISTA_CONTROLLER.php' );
+			
+			
+		} else {//Si recibe datos los recoge y mediante las funcionalidad de PISTA_MODEL inserta los datos
+			$PISTA = get_data_form();//Variable que almacena un objecto PISTA(modelo) con los datos recogidos
+			$respuesta = $PISTA->ADD();//Variable que almacena la respuesta de la inserción
+			//Crea la vista con la respuesta y la ruta para volver
+			new MESSAGE( $respuesta, '../Controllers/PISTA_CONTROLLER.php' );
+		}
+		//Finaliza el bloque
+		break;
+	case 'DELETE'://Caso borrar
+		if ( !$_POST ) {//Si no se han recibido datos se envia a la vista del formulario DELETE
+
+		
+				//Variable que recoge un objecto model con solo el login
+				$PISTA = new PISTA_MODEL( $_REQUEST[ 'idPista' ], '', '', '', '', '', '', '', '');
+				//Variable que almacena el relleno de los datos utilizando el login
+				$valores = $PISTA->RellenaDatos( $_REQUEST[ 'idPista' ] );
+
+            
+				//Crea una vista delete para ver la tupla
+				new PISTA_DELETE( $valores);
+			
+			//Si recibe valores ejecuta el borrado
+		} else {//Si recibe datos los recoge y mediante las funcionalidad de PISTA_MODEL borra los datos
+			//Variable que almacena un objecto PISTA(modelo) con los datos recogidos de los atributos
+			$PISTA = get_data_form();
+			//Variable que almacena la respuesta de realizar el borrado
+			$respuesta = $PISTA->DELETE();
+			//crea una vista mensaje con la respuesta y la dirección de vuelta
+			new MESSAGE( $respuesta, '../Controllers/PISTA_CONTROLLER.php' );
+		}
+		//Finaliza el bloque
+		break;
+	case 'EDIT'://Caso editar	
+		if ( !$_POST ) {//Si no se han recibido datos se envia a la vista del formulario EDIT
+
+			
+			$PISTA = new PISTA_MODEL( $_REQUEST[ 'idPista' ], '', '', '', '', '', '', '','');
+			//Variable que almacena un objecto PISTA(modelo) con los datos de los atibutos rellenados a traves de login
+			$valores = $PISTA->RellenaDatos( $_REQUEST[ 'idPista' ] );
+
+			//Muestra la vista del formulario editar
+			new PISTA_EDIT( $valores);
+			
+			
+			//Si se reciben valores
+		} else {
+			//Variable que almacena un objecto PISTA model de los datos recogidos
+			$PISTA = get_data_form();
+			//Variable que almacena la respuesta de la edición de los datos
+			$respuesta = $PISTA->EDIT();
+			//crea una vista mensaje con la respuesta y la dirección de vuelta
+			new MESSAGE( $respuesta, '../Controllers/PISTA_CONTROLLER.php' );
+		}
+		//Fin del bloque
+		break;
+	case 'SEARCH'://Caso buscar
+		if ( !$_POST ) {//Si no se han recibido datos se envia a la vista del formulario SEARCH
+			
+		//Si se reciben datos	
+		} else {
+			
+			//Variable que almacena los datos recogidos de los atributos
+			$PISTA = get_data_form();
+			//Variable que almacena el resultado de la busqueda
+			$datos = $PISTA->SEARCH();
+			//Variable que almacena array con el nombre de los atributos
+			$lista = array( 'idPista','Hora','Fecha','Disponibilidad');
+			//Creacion de la vista showall con el array $lista, los datos y la ruta de vuelta
+		
+				new PISTA_SHOWCURRENT( $lista, $datos );
+			
+			
+		}
+		//Final del bloque
+		break;
+	case 'SHOWCURRENT'://Caso showcurrent
+		if($_SESSION['grupo'] == 'Admin'){//miramos si el PISTA es administrador
+					//Variable que almacena un objeto PISTA model con el login
+		           $PISTA = new PISTA_MODEL( $_REQUEST[ 'login' ], '', '', '', '', '', '', '','');
+		//Variable que almacena los valores rellenados a traves de login
+		           $valores = $PISTA->RellenaDatos( $_REQUEST[ 'login' ] );
+		           //Creación de la vista showcurrent
+		           new PISTA_SHOWCURRENT( $valores );
+			}else{
+				new MESSAGE( 'El PISTA no tiene los permisos necesarios', '../Controllers/PISTA_CONTROLLER.php' );
+			}
+		
+		//Final del bloque
+		break;
+	default: //Caso que se ejecuta por defecto
+			if($_SESSION['grupo'] == 'Admin'){//miramos si el PISTA es administrador
+						if ( !$_POST ) {//Si no se han recibido datos 
+						$PISTA = new PISTA_MODEL( '', '', '', '', '', '', '', '', '');//Variable que almacena la un objeto del modelo PISTA
+						//Si se reciben datos
+						} else {
+							$PISTA = get_data_form();//Variable que almacena los valores de un objeto PISTA_MODEL
+						}
+						//Variable que almacena los datos de la busqueda
+						$datos = $PISTA->SEARCH();
+						//Variable que almacena array con el nombre de los atributos
+						$lista = array( 'login','DNI','Nombre','Apellidos','Correo','idGrupo');
+						
+						new PISTA_SHOWALL( $lista, $datos);//nos muestra una vista showall con todos los permisos
+			
+   				}else{//en el caso de que el PISTA no tenga permisos le sale una vista vacía
+				new PISTA_DEFAULT();
+			}
+			
+}
+
+?>
---
+++ PadelApp/app/Views/PISTAS/PISTA_SHOWALL.php
@@ -0,0 +1,95 @@
+<?php
+
+class Pista_Showall{
+	
+    //es el constructor de la clase Pista_Showall
+	function __construct( $lista, $datos) {
+		$this->lista = $lista;//pasamos los campos de la tabla PISTAS
+		$this->datos = $datos;//pasamos los valores de cada campo
+
+		$this->render($this->lista,$this->datos);//llamamos a la función render donde se mostrará el formulario SHOWALL con los campos correspondientes
+	}
+	function render($lista,$datos){
+		$this->lista = $lista;//pasamos los campos de la tabla PISTAS
+		$this->datos = $datos;//pasamos los valores de cada campo
+
+		include '../Locales/Strings_' . $_SESSION[ 'idioma' ] . '.php';//incluimos los strings de idiomas, para que la página pueda estar en español,inglés y galego
+
+  
+	include '../Views/Header.php';//incluimos la cabecera			
+
+?>
+<div class="seccion">
+			<h2>
+				<?php echo $strings['Tabla de datos'];?>
+			</h2>
+			<table>
+				<caption style="margin-bottom:10px;">
+					<form action='../Controllers/PISTA_CONTROLLER.php'>
+
+						<button type="submit" name="action" value="ADD"><img src="../Views/icon/añadir.png" alt="AÑADIR" /></button>
+
+					</form>
+				</caption>
+				<tr>
+<?php
+					foreach ( $lista as $atributo ) { //este bucle devolverá cada uno de los campos de la tabla PISTA
+?>
+					<th>
+						<?php echo $strings[$atributo]?>
+					</th>
+<?php
+					}
+?>
+					<th colspan="4" >
+						<?php echo $strings['Opciones']?>
+					</th>
+
+				</tr>
+<?php
+				while ( $fila = mysqli_fetch_array( $this->datos ) ) { //este bucle va a devolver todas las tuplas de la tabla PISTA de la base de datos
+?>
+				<tr>
+<?php
+					foreach ( $lista as $atributo ) { //este bucle va a ir devolviendo el valor de cada campo de la tabla PISTA
+?>
+					<td>
+<?php 
+							echo $fila[ $atributo ];
+
+?>
+					</td>
+<?php
+					}
+?>
+					<td>
+						<form action="../Controllers/PISTA_CONTROLLER.php" method="get" style="display:inline" >
+							<input type="hidden" name="login" value="<?php echo $fila['login']; ?>">
+							
+								<button type="submit" name="action" value="EDIT" ><img src="../Views/icon/modificar.png" alt="<?php echo $strings['Modificar']?>" width="20" height="20" /></button>
+						
+					<td>
+						
+								<button type="submit" name="action" value="DELETE" ><img src="../Views/icon/eliminar.png" alt="<?php echo $strings['Eliminar']?>" width="20" height="20" /></button>
+					
+					<td>
+								<button type="submit" name="action" value="SEARCH"><img src="../Views/icon/buscar.png" alt="BUSCAR" /></button>		
+					<td>	
+					
+						</form>
+				    <td>
+													
+				</tr>
+<?php
+				}
+?>
+			</table>
+			<form action='../Controllers/PISTA_CONTROLLER.php' method="post">
+				<button type="submit"><img src="../Views/icon/atras.png" alt="<?php echo $strings['Atras']?>" /></button>
+			</form>
+		</div>
+<?php
+		include '../Views/Footer.php';//incluimos el pie de la página
+		}
+		}
+?>
