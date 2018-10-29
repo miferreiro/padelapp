5953cbb83337cedf68354ad582c7fa7dd18d6c81
DESKTOP-M5AEJ9Q\david <davurin76@gmail.com>
2018-10-29 20:39:45 +0100

    PROMOS DEL AND SHOWCURRENT

--- PadelApp/app/Views/PROMOCIÓN/PROM_DELETE.php
+++ PadelApp/app/Views/PROMOCIÓN/PROM_DELETE.php
@@ -1,10 +1,63 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Documento sin título</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+class PROM_DELETE {
+
+	function __construct( $valores) { 
+		$this->valores = $valores;
+
+		$this->render( $this->valores);
+	}
+
+	function render( $valores) { 
+		$this->valores = $valores;
+	
+
+		include '../Locales/Strings_' . $_SESSION[ 'idioma' ] . '.php';
+		include '../Views/Header.php';
+?>
+		<div class="seccion" align="center">
+			<h2>
+				<?php echo $strings['Tabla de borrado'];?>
+			</h2>
+			<div class="col-sm-4">
+			<table class="table table-sm">
+				<thead class="thead-light">
+				<tr>
+					<th>
+						<?php echo $strings['Fecha'];?>
+					</th>
+					<td>
+						<?php echo $this->valores['Fecha']?>
+					</td>
+				</tr>
+				<tr>
+					<th>
+						<?php echo $strings['Hora'];?>
+					</th>
+					<td>
+						<?php echo $this->valores['Hora']?>
+					</td>
+				</tr>
+				</thead>
+			</table>
+			</div>	
+<?php 
+			echo $strings['¿Está seguro de que quiere borrar esta tupla de la tabla?'];
+?>
+			<div>
+			<form action="../Controllers/PROM_CONTROLLER.php" method="post" style="display: inline" >
+				<input type="hidden" name="Fecha" value="<?php echo $fila['Fecha']; ?>">
+				<input type="hidden" name="Hora" value="<?php echo $fila['Hora']; ?>">
+
+				<button id ="buttonBien" type="submit" name="action" value="DELETE" ><img src="../Views/icon/confirmar.png" alt="<?php echo $strings['Confirmar'] ?>"/></button>
+			</form>
+			<form action='../Controllers/PROM_CONTROLLER.php' method="post" style="display: inline">
+				<button id ="buttonBien" type="submit"><img src="../Views/icon/cancelar.png" alt="<?php echo $strings['Atras'] ?>"/></button>
+			</form>
+			</div>
+		</div>
+<?php           
+		include '../Views/Footer.php';               
+         }   
+	}
+?>
--- PadelApp/app/Views/PROMOCIÓN/PROM_SHOWCURRENT.php
+++ PadelApp/app/Views/PROMOCIÓN/PROM_SHOWCURRENT.php
@@ -1,10 +1,54 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Documento sin título</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+class PROM_SHOWCURRENT {
+
+
+	function __construct( $lista ) {
+		$this->lista = $lista;
+		$this->render( $this->lista );
+	}
+
+	function render( $lista ) { 
+		$this->lista = $lista;
+		include '../Locales/Strings_' . $_SESSION[ 'idioma' ] . '.php';
+		include '../Views/Header.php';
+?>
+<div class="seccion" align="center">
+		<h2>
+			<?php echo $strings['Vista detallada'];?>
+		</h2>
+		<div class="col-md-4">
+		<table class="table table-sm">
+			<thead class="thead-light">
+			<tr>
+				<th>
+					<?php echo $strings['Fecha'];?>
+				</th>
+				<td>
+					<?php echo $this->lista['Fecha']?>
+				</td>
+			</tr>
+
+			<tr>
+				<th>
+					<?php echo $strings['Hora'];?>
+				</th>
+				<td>
+					<?php echo $this->lista['Hora']?>
+				</td>
+			</tr>
+						
+			
+			</thead>
+		</table>
+	</div>
+				<form action='../Controllers/PROM_CONTROLLER.php' method="post">
+					<button id ="buttonBien" type="submit"><img src="../Views/icon/atras.png" alt="<?php echo $strings['Atras'] ?>" /></button>
+				</form>
+			
+</div>
+<?php
+		include '../Views/Footer.php';
+	}
+}
+?>
