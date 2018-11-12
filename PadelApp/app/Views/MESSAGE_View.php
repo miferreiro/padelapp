62c4fecb633375ca2161c8bca3601acead567458
miferreiro <miguel.ferreiro.diaz@gmail.com>
2018-11-12 14:05:46 +0100

    add "Documentacion"

--- PadelApp/app/Views/MESSAGE_View.php
+++ PadelApp/app/Views/MESSAGE_View.php
@@ -1,26 +1,19 @@
 <?php
 
-/*  Archivo php
-	Nombre: mensaje.php
-	Autor: 	Miguel Ferreiro
-	Fecha de creación: 9/10/2017 
-	Función: vista de un mensaje(message) realizada con una clase donde se muestra el mensaje deseado
-*/
 
-//es la clase que muestra un mensaje y un enlace para volver atras
+
 class MESSAGE { 
 
-    //constructor de la clase
+    
 	function __construct( $text, $ruta ) { 
-		$this->text = $text;//pasamos el texto a mostrar
-		$this->ruta = $ruta;//pasamos la ruta para volver atras
-		$this->render();//llamamos a la función para que muestre el mensaje y vuelta atras
+		$this->text = $text;
+		$this->ruta = $ruta;
+		$this->render();
 	}
-//función para que muestre el mensaje y vuelta atras
 	function render() {
 
-		include '../Locales/Strings_' . $_SESSION[ 'idioma' ] . '.php';//incluimos todos los strings de los idiomas:ingles,español y galego
-		include '../Views/Header.php';//incluimos la cabecera
+		include '../Locales/Strings_' . $_SESSION[ 'idioma' ] . '.php';
+		include '../Views/Header.php';
 ?>
 	<div class="seccion" align="center">
 	<div class="col-md-2">
@@ -29,7 +22,7 @@ class MESSAGE {
 		<br>
 		<br>
 		<?php
-			echo $strings[$this->text]; // se muestra por pantalla el texto?>
+			echo $strings[$this->text];?>
 
 		
 		<br>
@@ -43,7 +36,7 @@ class MESSAGE {
 	</div>
 	</div>
 <?php
-	include '../Views/Footer.php';//incluimos el footer
+	include '../Views/Footer.php';
 	}
 }
 ?>
--- PadelApp/app/Views/users_index_View.php
+++ PadelApp/app/Views/users_index_View.php
@@ -1,28 +1,14 @@
 <?php
-/*  Archivo php
-	Nombre: users_index_View.php
-	Autor: 	Miguel Ferreiro
-	Fecha de creación: 23/10/2017 
-	Función: vista de los elementos que va a contar la sesión del usuario, realizada con una clase
-*/
-
-//es la clase del inicio de la página
 class Index {
 
-    //es el constructor
 	function __construct(){
-		
-		$this->render();//llamamos a la función para que muestre la vista
+		$this->render();
 	}
 
-    //función que muestra la vista
 	function render(){
-	
-		include '../Locales/Strings_SPANISH.php';//incluimos  el idioma español
-		include '../Views/Header.php';//incluimos la cabecera
-		include '../Views/Footer.php';//incluimos el footer
+		include '../Locales/Strings_SPANISH.php';
+		include '../Views/Header.php';
+		include '../Views/Footer.php';
 	}
-
 }
-
 ?>
