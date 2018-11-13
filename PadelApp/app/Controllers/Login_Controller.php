7e2f421fbf7d98e7bd156d51bb241e72facc8687
miferreiro <miguel.ferreiro.diaz@gmail.com>
2018-11-13 01:03:38 +0100

    Documentacion

--- PadelApp/app/Controllers/Login_Controller.php
+++ PadelApp/app/Controllers/Login_Controller.php
@@ -1,37 +1,24 @@
 <?php
-/*
-	Archivo php
-	Nombre: Login_Controller.php
-	Autor: Miguel Ferreiro
-	Fecha creación: 23/10/2017 
-	Función: controlador que realiza las operaciones necesarias para realizar un logeo correcto de un usuario
-*/
-session_start();//se inicia la sesión
-if(!isset($_REQUEST['login']) && !(isset($_REQUEST['password']))){//mira si no existe el login y no existe la password
-	//Incluye la vista login
+
+session_start();
+if(!isset($_REQUEST['login']) && !(isset($_REQUEST['password']))){
+	
 	include '../Views/LOGIN_View.php';
-	//Variable que almacena un objecto de login
 	$login = new Login();
 }
-else{//si existe el y la password
-	//Incluye la función para conectarse a la base de datos
+else{
 	include '../Functions/BdAdmin.php';
 
-	//Incluye el acceso al modelo de datos
 	include '../Models/USUARIO_MODEL.php';
-	$usuario = new USUARIO_MODEL($_REQUEST['login'],$_REQUEST['password'], '', '', '', '', '', '','');//Variable que almacena un objeto del modelo USUARIOS_MODEL
-	$respuesta = $usuario->login();//Variable que almacena la respuesta al método login de USARIOS_MODEL para saber si está bien logeado
+	$usuario = new USUARIO_MODEL($_REQUEST['login'],$_REQUEST['password'], '', '', '', '', '', '','');
+	$respuesta = $usuario->login();
 
-	//Si existe el usuario se devuelve true y le asignamos a la variable de sesion el valor del login
 	if ($respuesta == 'true'){
-		//session_start();//se inicia la sesión
-		$_SESSION['login'] = $_REQUEST['login'];//le asignamos a la variable de sesión del login el login que se introdujo
-		
+		$_SESSION['login'] = $_REQUEST['login'];	
 		$_SESSION['tipo'] = $usuario->obtenerTipo();
 		$_SESSION['dni'] = $usuario->obtenerDni();
-		header('Location:../Controllers/DEFAULT_CONTROLLER.php');//se redirige al controlador de USUARIOS_CONTROLLER.php
+		header('Location:../Controllers/DEFAULT_CONTROLLER.php');
 	}
-	//Si no esta en la base de datos, se muestra la respuesta en la vista mensaje
 	else{
 	
 		//Incluye la vista de mensaje
--- PadelApp/app/Controllers/Registro_Controller.php
+++ PadelApp/app/Controllers/Registro_Controller.php
@@ -1,38 +1,30 @@
 <?php
-/*
-	Archivo php
-	Nombre: Register_Controller.php
-	Autor: Miguel Ferreiro
-	Fecha de creación: 23/10/2017 
-	Función: controlador que realiza las operaciones necesarias para realizar un registro correcto de un nuevo usuario
-*/
-session_start();//se inicia la sesión
-include_once '../Locales/Strings_'.$_SESSION['idioma'].'.php';//incluimos los strings para poder cambiar de idioma(español,galego,inglés)
 
-if(!isset($_POST['login'])){//Si no se han recibido datos del login
-	include '../Views/REGISTRO_View.php';//incluimos la vista para Registrarse
-	$register = new Register();//Variable que almacena un objeto de tipo Registro para registrarnos
+session_start();
+include_once '../Locales/Strings_'.$_SESSION['idioma'].'.php';
+
+if(!isset($_POST['login'])){
+	include '../Views/REGISTRO_View.php';
+	$register = new Register();
 }
 else{
 		
-	include '../Models/USUARIO_MODEL.php';//incluye el contenido del modelo USUARIO_MODEL
-	//Variable que almacena un objecto de usuarios model
-	$usuario = new USUARIO_MODEL($_REQUEST['login'],$_REQUEST['password'],$_REQUEST['Dni'],$_REQUEST['nombre'],$_REQUEST['apellidos'],$_REQUEST['telefono'],$_REQUEST['sexo'],'Deportista');//creamos un objeto del modelo USUARIO_MODEL donde le pasamos todos los datos del usuario registrado
-	//Variable que almacena la respuesta de si esta existe el login o no para poder registrarse
+	include '../Models/USUARIO_MODEL.php';
+	
+	$usuario = new USUARIO_MODEL($_REQUEST['login'],$_REQUEST['password'],$_REQUEST['Dni'],$_REQUEST['nombre'],$_REQUEST['apellidos'],$_REQUEST['telefono'],$_REQUEST['sexo'],'Deportista');
+	
 	$respuesta = $usuario->Register();
 
-	//Si no existe el login en la base de datos
 	if ($respuesta == 'true'){
-			$respuesta = $usuario->ADD();//Variable que almacena la respuesta de añadir este usuario a la base de datos
-		//Incluye la vista mensaje
-		include '../Views/MESSAGE_View.php';//incluimos la vista para que nos mandé un mensaje por pantalla
-		new MESSAGE($respuesta, '../index.php');//creamos un objeto de tipo mensaje que mostrara el mensaje que viene del modelo.
+			$respuesta = $usuario->ADD();
+
+		include '../Views/MESSAGE_View.php';
+		new MESSAGE($respuesta, '../index.php');
 	}
-	//Si existe en la base de datos
 	else{
-		//Incluye la vista mensaje
-		include '../Views/MESSAGE_View.php';//incluimos la vista para que nos mandé un mensaje por pantalla
-		new MESSAGE($respuesta, '../index.php');//creamos un objeto de tipo mensaje que mostrara el mensaje que viene del modelo.
+
+		include '../Views/MESSAGE_View.php';
+		new MESSAGE($respuesta, '../index.php');
 	}
 
 }
