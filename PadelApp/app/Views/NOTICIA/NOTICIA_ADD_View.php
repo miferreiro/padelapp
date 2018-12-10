3e3c93408cf03eb836fd08930a9f543ddc6af966
AlexVC14 <alexvolocid@gmail.com>
2018-12-10 11:12:43 +0100

    Noticia

---
+++ PadelApp/app/Views/NOTICIA/NOTICIA_ADD_View.php
@@ -0,0 +1,58 @@
+<?php
+
+class NOTICIA_ADD {
+
+	function __construct() {
+		$this->render();
+	}
+
+	function render() {
+		include '../Locales/Strings_' . $_SESSION[ 'idioma' ] . '.php';
+		include '../Views/Header.php';
+?>
+		<div class="seccion" align="center">
+			<h2>
+				<?php echo $strings['Formulario de inserción de noticia'];?>
+			</h2>
+			<form name="ADD" action="../Controllers/NOTICIA_CONTROLLER.php" method="post" enctype="multipart/form-data" onsubmit=""/> 
+			<div class="col-sm-4">
+			<table class="table table-sm">
+				<thead class="thead-light">
+					<tr>
+						<th class="formThTd">
+							<?php echo $strings['Titulo'];?>
+						</th>
+						<td class="formThTd"><input type="text" id="Titulo" name="Titulo" placeholder="<?php echo $strings['Escriba aqui...']?>" value="" maxlength="25" size="25" required />
+					</tr>
+					<tr>
+						<th class="formThTd">
+							<?php echo $strings['Contenido'];?>
+						</th>
+						<td class="formThTd">
+							<textarea id="Contenido" name="Contenido" rows="10" cols="50" required>
+							</textarea>
+					</tr>
+					<tr>
+						<th class="formThTd">
+							<?php echo $strings['Foto'];?>
+						</th>
+						<td class="formThTd"><input type="file" id="fotopersonal" name="fotopersonal"/>
+					</tr>
+					
+					<tr align="center">
+						<td colspan="2">
+							<button id ="buttonBien" type="submit" name="action" value="ADD"><img src="../Views/icon/accept_big.png" alt="<?php echo $strings['Confirmar formulario']?>" /></button>
+			</form>
+						<form action='../Controllers/USUARIO_CONTROLLER.php' method="post" style="display: inline">
+							<button id ="buttonBien" type="submit"><img src="../Views/icon/back_big2.png" alt="<?php echo $strings['Atras']?>" /></button>
+						</form>
+					</tr>
+				</thead>
+				</table>
+				</div>
+		</div>
+<?php
+		include '../Views/Footer.php';
+		}
+		}
+?>
---
+++ PadelApp/app/Views/NOTICIA/NOTICIA_SHOWALL_View.php
@@ -0,0 +1,96 @@
+<?php
+
+class NOTICIA_SHOWALL {
+
+	function __construct( $lista, $datos) {
+		$this->lista = $lista;
+		$this->datos = $datos;
+		$this->render($this->lista,$this->datos);
+	}
+	
+	function render($lista,$datos){
+		$this->lista = $lista;
+		$this->datos = $datos;
+
+		include '../Locales/Strings_' . $_SESSION[ 'idioma' ] . '.php';
+  
+	include '../Views/Header.php';		
+?>
+
+		<div class="seccion" align="center">
+			<h2>
+				<?php echo $strings['Noticias'];?>
+			</h2>
+			<div class="col-md-6">
+			<table class="table" id="mydatatableUsuarios">
+				<thead class="thead-light">
+				
+				<tr>
+<?php
+					foreach ( $lista as $atributo ) { 
+?>
+						<th>
+							<?php echo $strings[$atributo]?>
+						</th>
+<?php
+					}
+?>
+					<th>
+						<?php echo $strings['Opciones']?>
+					</th>
+
+				</tr>
+				</thead>
+<?php
+				while ( $fila = mysqli_fetch_array( $this->datos ) ) { 
+?>
+				<tr>
+<?php
+					foreach ( $lista as $atributo ) { 
+?>
+					<td>
+<?php 
+							echo $fila[ $atributo ];
+?>
+					</td>
+<?php
+					}
+?>
+					
+					<td>
+						<form action="../Controllers/NOTICIA_CONTROLLER.php" method="get" style="display:inline" >
+							<input type="hidden" name="Titulo" value="<?php echo $fila['Titulo']; ?>">
+										
+								<button id ="buttonBien" type="submit" name="action" value="DELETE" ><img src="../Views/icon/delete_big.png" alt="<?php echo $strings['Eliminar']?>" width="20" height="20" /></button>				
+
+						</form>						
+					</td>								
+				</tr>
+<?php
+				}
+?>
+				
+				
+			</table>
+				
+				<tr align="center">
+						<td colspan="2">
+					<form action='../Controllers/USUARIO_CONTROLLER.php'>
+
+						<button id ="buttonBien"  type="submit" name="action" value="SEARCH"><img src="../Views/icon/search_big.png" alt="BUSCAR" /></button>	
+
+						<button id ="buttonBien"  type="submit" name="action" value="ADD"><img src="../Views/icon/add_big.png" alt="AÑADIR" /></button>
+					</form>
+					</td>
+				</tr>
+			
+			</div>
+			<form action='../Controllers/DEFAULT_CONTROLLER.php' method="post">
+				<button id ="buttonBien" type="submit"><img src="../Views/icon/back_big2.png" alt="<?php echo $strings['Atras']?>" /></button>
+			</form>
+		</div>
+<?php
+		include '../Views/Footer.php';
+		}
+	}
+?>
